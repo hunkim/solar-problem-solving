87d897eb9dab3d8a7baedd94331c82d346ef6060
Sung Kim <hunkim@gmail.com>
2024-05-27 14:21:40 +0900

    Added app

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 # from https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps
 
 import streamlit as st
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
 from langchain_upstage import ChatUpstage as Chat
 from langchain_upstage import UpstageGroundednessCheck
 
 from langchain_core.output_parsers import StrOutputParser
 from langchain_upstage import UpstageLayoutAnalysisLoader
 from langchain_core.prompts import PromptTemplate
+from concurrent.futures import ThreadPoolExecutor
+
+
+import threading
+
 
 import tempfile, os, base64
 
@@ -21,6 +29,7 @@ agent_results = {}
 
 
 retries = 3
+
 
 def get_agent_response(agent, context):
     comon_instruction = """Apply the analysis technique to the context. 
@@ -93,47 +102,85 @@ def GC_response(agent, context, response):
 def run_follow_up():
     total_agent_count = len(default_agents)
     for i, agent in enumerate(default_agents):
-        with st.status(
-            f"[{(i+1)}/{total_agent_count}] Running {agent['name']} ...",
-            expanded=True,
-        ):
-            place = st.empty()
-            place_info = st.empty()
-            for i in range(retries):
-                response = place.write_stream(get_agent_response(agent, context))
-                place_info.info(f"Checking the response. Trial {i+1} ...")
-                if GC_response(agent, context, response):
-                    place_info.success("Agent response is good!")
-                    break
+        if i == 0:
+            response = gen_analysis(total_agent_count, i, agent)
+        elif i == len(default_agents) - 1:
+            # Last case
+            gen_diagram(agent, response, place_digram_status, place_digram)
+        else:
+            prev_agent = default_agents[i - 1]
+            prev_response = agent_results[prev_agent["name"]]["response"]
 
-                place_info.info("The response is not good. Let me retry ...")
-            else:
-                place_info.error("Please double check the response!")
+            place_digram_status = st.empty()
+            place_digram = st.empty()
+
+            with ThreadPoolExecutor(max_workers=2) as executor:
+                ctx = get_script_run_ctx()
+                executor.submit(
+                    gen_diagram,
+                    prev_agent,
+                    prev_response,
+                    place_digram_status,
+                    place_digram,
+                    ctx,
+                )
+                executor.submit(gen_analysis, total_agent_count, i, agent, ctx)
+
+                # for t in executor._threads:
+                #    add_script_run_ctx(t)
+                # gen_diagram(prev_agent, prev_response, place_digram_status, place_digram)
+
+
+def gen_analysis(total_agent_count, i, agent, ctx=None):
+    if ctx:
+        add_script_run_ctx(threading.currentThread(), ctx)
+
+    with st.status(
+        f"[{(i+1)}/{total_agent_count}] Running {agent['name']} ...",
+        expanded=True,
+    ):
+        place = st.empty()
+        place_info = st.empty()
+        for i in range(retries):
+            response = place.write_stream(get_agent_response(agent, context))
+            place_info.info(f"Checking the response. Trial {i+1} ...")
+            if GC_response(agent, context, response):
+                place_info.success("Agent response is good!")
+                break
+
+            place_info.info("The response is not good. Let me retry ...")
+        else:
+            place_info.error("Please double check the response!")
 
             # store the agent response
-            agent_results[agent["name"]] = {"response": response}
+        agent_results[agent["name"]] = {"response": response}
+    return response
 
-        img = None
-        code = None
-        with st.status(f"Diagram generation for {agent['name']} ..."):
-            for i in range(retries):
-                try:
-                    code = st.write_stream(get_agent_code(agent, context, response))
-                    st.info("Generating the diagram ...")            
-                    img = code2img(code)
-                    if img:
-                        st.image(img, caption="Generated Diagram")
-                        break
-                    st.warning("Let me retry ...")
-                except Exception as e:
-                    st.error(f"Error: {e}")
-                    st.warning("Please try again!")
 
-        if img:
-            st.image(img, caption="Generated Diagram")
-            agent_results[agent["name"]]["code"] = code
-            agent_results[agent["name"]]["img"] = img
-            
+def gen_diagram(agent, response, place_digram_status, place_digram, ctx=None):
+    if ctx:
+        add_script_run_ctx(threading.currentThread(), ctx)
+
+    img = None
+    code = None
+    with place_digram_status.status(f"Diagram generation for {agent['name']} ..."):
+        for i in range(retries):
+            try:
+                code = st.write_stream(get_agent_code(agent, context, response))
+                st.info("Generating the diagram ...")
+                img = code2img(code)
+                if img:
+                    st.image(img, caption="Generated Diagram")
+                    break
+                st.warning("Let me retry ...")
+            except Exception as e:
+                st.error(f"Error: {e}")
+                st.warning("Please try again!")
+
+    if img:
+        place_digram.image(img, caption="Generated Diagram")
+        agent_results[agent["name"]]["code"] = code
+        agent_results[agent["name"]]["img"] = img
 
 
 if __name__ == "__main__":
@@ -159,7 +206,9 @@ if __name__ == "__main__":
                     with open(file_path, "wb") as f:
                         f.write(uploaded_file.getvalue())
 
-                    layzer = UpstageLayoutAnalysisLoader(file_path, split="page", use_ocr=True)
+                    layzer = UpstageLayoutAnalysisLoader(
+                        file_path, split="page", use_ocr=True
+                    )
                     # For improved memory efficiency, consider using the lazy_load method to load documents page by page.
                     docs = layzer.load()  # or layzer.lazy_load()
                     st.write(f"Loaded {len(docs)} pages from {uploaded_file.name}")
@@ -173,23 +222,17 @@ if __name__ == "__main__":
         for agent, result in agent_results.items():
             response = result["response"]
             download_context += f"\n\n## {agent}\n{response}\n\n"
-            download_context += "### Generated Diagram\n"
-            download_context += f"\n\n{result['code']}\n\n"
-            # insert image as base64 first generate base64
-            img_base64_txt = base64.b64encode(result["img"]).decode("utf-8")
-            download_context += (
-                f"![Generated Diagram](data:image/png;base64,{img_base64_txt})\n\n"
-            )
 
-            if "code" in result:    
+            if "code" in result:
                 download_context += "### Generated Diagram\n"
                 download_context += f"\n\n{result['code']}\n\n"
 
-            if "img" in result:    
+            if "img" in result:
                 # insert image as base64 first generate base64
                 img_base64_txt = base64.b64encode(result["img"]).decode("utf-8")
-                download_context += f"![Generated Diagram](data:image/png;base64,{img_base64_txt})\n\n"
-
+                download_context += (
+                    f"![Generated Diagram](data:image/png;base64,{img_base64_txt})\n\n"
+                )
 
         st.download_button(
             label="Download Results",
